8b2d965723d4b1a490432878cc33b9dc30a5816b
Tanja Menkovic <tmenkovi@c1r2p4.hive.fi>
2024-07-19 11:07:28 +0300

    checking arguments

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
-#include "../headers/minirt.h"
+#include "headers/minirt.h"
 
-int main()
+static void check_args(int argc, char **argv)
 {
+    char    *filename;
+
+    if (argc != 2)
+    {
+        error_msg("number of arguments is not valid!\n", "Usage: ./miniRT <scene_file.rt>\n");
+        exit(EXIT_FAILURE);
+    }
+    if (ft_strchr(argv[1], '.') == NULL || ft_strcmp(ft_strchr(argv[1], '.'), ".rt") != 0)
+    {
+        error_msg("argument is not correct!\n", "Usage: ./miniRT <scene_file.rt>\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    check_args(argc, argv);
 
     return (0);
 }
