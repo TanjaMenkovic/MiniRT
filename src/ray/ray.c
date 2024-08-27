08c5584d9a01b38ede892efca8a2f2d36ec8fa6e
Tanja Menkovic <tmenkovi@c1r3p8.hive.fi>
2024-08-27 13:30:14 +0300

    ray norm

--- src/ray/ray.c
+++ src/ray/ray.c
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ray.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmenkovi <tmenkovi@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/27 13:29:23 by tmenkovi          #+#    #+#             */
+/*   Updated: 2024/08/27 13:29:24 by tmenkovi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../headers/ray.h"
 
-t_ray   init_ray(t_vector s, t_vector d)
+t_ray	init_ray(t_vector s, t_vector d)
 {
-    t_ray   r;
+	t_ray	r;
 
-    r.start = s;
-    r.direction = d;
-    r.t = RAY_MAX;
-    return (r);
+	r.start = s;
+	r.direction = d;
+	r.t = RAY_MAX;
+	return (r);
 }
 
-t_vector   ray_point(t_ray r, float t)
+t_vector	ray_point(t_ray r, float t)
 {
-    return (vec_add(r.start, vec_mult(r.direction, t)));
+	return (vec_add(r.start, vec_mult(r.direction, t)));
 }
